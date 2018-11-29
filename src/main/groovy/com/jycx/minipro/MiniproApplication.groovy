9d67c369e5d26d7d732c4594b6440d9de23ecb6a
冯禹铭 <fengyuming@meituan.com>
2018-11-29 20:05:57 +0800

    first commit

---
+++ src/main/groovy/com/jycx/minipro/MiniproApplication.groovy
@@ -0,0 +1,12 @@
+package com.jycx.minipro
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MiniproApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run MiniproApplication, args
+	}
+}
---
+++ src/test/groovy/com/jycx/minipro/MiniproApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.jycx.minipro
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class MiniproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
